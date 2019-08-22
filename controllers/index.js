513376125790673c57369302d9362230cc5ea3f1
Unknown <julian_decastro@dlsu.edu.ph>
2019-08-23 01:04:17 +0800

    changed index else redirect to render home

--- controllers/index.js
+++ controllers/index.js
@@ -26,7 +26,7 @@ router.get("/", function (req, res) {
         res.render("index")
     } else {
         Post.getAll().then((posts) => {
-            res.redirect("/")
+            res.render("home")
         })
     }
 })
